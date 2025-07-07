5c3421982424b2617604356c797d7b9973c503a6
추승주 <162384860+Seungju08@users.noreply.github.com>
2025-07-07 18:46:13 +0900

    ⚙️ project setting [20250707]

--- README.md
+++ README.md
@@ -1,16 +1,7 @@
 # dooit
 
-A new Flutter project.
+This is an app that gives you motivation for health and exercise.
 
-## Getting Started
+## dooit Figma prototype
 
-This project is a starting point for a Flutter application.
-
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
-
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+https://www.figma.com/proto/39d0EDLyStoZ2CQkktGlnT/Capstone-Dooit?node-id=0-1&t=nQnbG4gTJclFqCNH-1
--- lib/presentation/screens/splash_screen.dart
+++ lib/presentation/screens/splash_screen.dart
@@ -1,13 +1,33 @@
 import 'package:flutter/material.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       body: Center(
-
+        child: Text(
+          'Dooit',
+          style: TextStyle(
+            color: Color(0xff171717),
+            fontSize: 40,
+            fontFamily: 'Pretendard',
+            fontWeight: FontWeight.w800,
+          ),
+        ),
       ),
     );
   }
